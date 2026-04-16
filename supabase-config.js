acb4595213deb115a82569a66e1c55533743774e
Halima Bulama ladan <heilleys@gmail.com>
2026-04-16 19:39:06 -0400

    Update Supabase URL and anonKey

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.CHORES_SUPABASE_CONFIG = window.CHORES_SUPABASE_CONFIG || {
   enabled: true,
-  url: "https://msorvelbzhuucboavlix.supabase.co",
-  anonKey: "sb_publishable_y-olA2S-X0RJQASzVYaOig_4KZR1_S_",
+  url: "https://uhaegkwipknboqxjwtch.supabase.co",
+  anonKey: "sb_publishable_QR7p9INce5n9nx_D5n-nvQ_gtOgrY-D",
 };
